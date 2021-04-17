36463cef791682819dc8a73d3b962b2a63fc5703
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-17 09:50:20 -0400

    remove reference to DownloadButton

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import DownloadButton from '../components/DownloadButton';
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {

97c6b41fda3c6f698ea3926383702af8297cabfb
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-17 09:29:21 -0400

    use downloadBtn & sidebar; remove footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import DownloadButton from '../components/DownloadButton';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
@@ -8,8 +10,13 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        <Sidebar />
+      </nav>
 
       <main className={styles.main}>
+        <DownloadButton apiRoute={"/api/hello"} filename={"blah.txt"} />
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -49,17 +56,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }

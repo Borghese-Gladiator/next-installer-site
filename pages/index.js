9f1431067d6cd3d064773882612cbecaaee93213
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-17 10:37:43 -0400

    update download to be a button & style text & remove unused

--- pages/index.js
+++ pages/index.js
@@ -9,47 +9,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <a href="/TS_Current_Resume.pdf">Click here for your file</a>
+        <a href="/TS_Current_Resume.pdf" style={{
+          backgroundColor: 'red',
+          color: 'white',
+          padding: '1em 1.5em',
+          textDecoration: 'none',
+          textTransform: 'uppercase'
+        }}>
+          Click here for your file</a>
         {/*<a href="/my-electron-app-0.1.0-Setup.exe">Click here for your EXE file</a>*/}
 
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to {`APP_NAME`}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Notepad++ is a free (as in “free speech” and also as in “free beer”) source code editor and Notepad replacement that supports several languages. Running in the MS Windows environment, its use is governed by GNU General Public License.
         </p>
+        <p className={styles.description}>
+          Based on the powerful editing component Scintilla, Notepad++ is written in C++ and uses pure Win32 API and STL which ensures a higher execution speed and smaller program size. By optimizing as many routines as possible without losing user friendliness, Notepad++ is trying to reduce the world carbon dioxide emissions. When using less CPU power, the PC can throttle down and reduce power consumption, resulting in a greener environment.
+        </p>
+        
+        {/* USAGE => transform: scale(<percentage of original>) */}
+        <img
+          src={'/big_brain.png'}
+          alt="super big brain"
+          style={{
+            transform: 'scale(1.0)'
+          }}
+        />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.description}>
+          I hope you enjoy Notepad++ as much as I enjoy coding it.
+        </p>
       </main>
 
     </div>

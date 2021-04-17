41d08b327906d3c25467874c5234a78492d3ad6c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-17 11:24:43 -0400

    initialize resources page

--- pages/resources.js
+++ pages/resources.js
@@ -9,47 +9,15 @@ export default function ResourcesPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <a href="/TS_Current_Resume.pdf">Click here for your file</a>
-        {/*<a href="/my-electron-app-0.1.0-Setup.exe">Click here for your EXE file</a>*/}
-
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Resources
         </h1>
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Source code repository is available on GitHub: <a href="https://github.com/Borghese-Gladiator/next-installer-site" target="_blank" rel="noopener noreferrer">https://github.com/Borghese-Gladiator/next-installer-site</a>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.description}>
+          Your pull requests will be welcome. However, they are not always accepted for diverse reasons. Here are the guidelines for your pull requests: <a href="https://github.com/notepad-plus-plus/notepad-plus-plus/blob/master/CONTRIBUTING.md" target="_blank" rel="noopener noreferrer">https://github.com/notepad-plus-plus/notepad-plus-plus/blob/master/CONTRIBUTING.md</a>
+        </p>
       </main>
 
     </div>

732009760eac4b57f6304cd942944f15de14050c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-17 10:53:46 -0400

    wrote up downloads page & styled it

--- pages/download.js
+++ pages/download.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Download.module.css'
 
 export default function Download() {
+  const downloadList = [
+    { filename: 'big_brain.png', text: 'Notepad++ 7.9.5 Release' },
+    { filename: 'big_brain.png', text: 'Notepad++ 7.9.5 Release' },
+    { filename: 'big_brain.png', text: 'Notepad++ 7.9.5 Release' },
+    { filename: 'big_brain.png', text: 'Notepad++ 7.9.5 Release' },
+    { filename: 'big_brain.png', text: 'Notepad++ 7.9.5 Release' },
+  ]
   return (
     <div>
       <Head>
@@ -9,46 +16,21 @@ export default function Download() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <a href="/TS_Current_Resume.pdf">Click here for your file</a>
-        {/*<a href="/my-electron-app-0.1.0-Setup.exe">Click here for your EXE file</a>*/}
-
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Downloads
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        
+        <div className={styles.downloadList}>
+          {downloadList.map((val, idx) => {
+            // idx at 0 === falsy, so every element afterwards gets a <hr> element - https://stackoverflow.com/questions/47881767/how-to-add-a-comma-in-array-map-after-every-element-except-last-element-in-react
+            const { text, filename } = val;
+            return (
+              <div>
+                {idx ? <hr /> : '' }
+                <a href={`/${filename}`}><h3>{text}</h3></a>
+              </div>
+            )
+          })}
         </div>
       </main>
 
